dc8fcb7ab7c7ea1a99d99951e1bfe17f108e6543
ahmedsaberellaithy <ahmedsaber.1992@gmail.com>
2023-01-11 12:02:34 +0200

    WIP: fixing swagger

---
+++ src/main/java/com/phone/messagingsystem/controllers/HealthController.java
@@ -0,0 +1,18 @@
+package com.phone.messagingsystem.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("health")
+@Slf4j
+public class HealthController {
+    @GetMapping()
+    public String healthCheck() {
+        return "I am alive";
+    }
+}
